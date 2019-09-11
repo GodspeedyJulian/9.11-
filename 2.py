71d2ae2ddb5d080e84f1184f3c846489a0bccd79
Julian Ye <50823612+GodspeedyJulian@users.noreply.github.com>
2019-09-11 13:04:25 +0800

    Update 2.py

--- 2.py
+++ 2.py
@@ -2,11 +2,11 @@ UpperCaseLetters = 0
 Digits = 0
 UserID = input("Your User ID: ")
 for i in range(len(UserID)):
-    if 65 <= ord(UserID[i]) <= 90:
-        UpperCaseLetters += 1
-    elif 48 <= ord(UserID[i]) <= 57:
-        Digits += 1
-if len(UserID)!=7 or UpperCaseLetters != 3 or Digits != 4:
-    print("not valid")
+    if 65<=ord(i)<=90:
+        upercase += 1
+    elif 0 <=int(i)<= 9:
+        digit += 1
+if upercase == 3 and digit == 4:
+    print('valid user ID')
 else:
-    print("valid")
+    print('invalid user ID')
